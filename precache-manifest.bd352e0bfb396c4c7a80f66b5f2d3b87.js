446e031d26d387d5d1e2a9a336f049a7f676e618
Oksana <myzove@mail.ru>
2020-08-19 21:01:03 +0300

    Updates

---
+++ precache-manifest.bd352e0bfb396c4c7a80f66b5f2d3b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8dd1e8cf9e8fb0f5dc94c09e66af33",
+    "url": "/spotify-react-redux/index.html"
+  },
+  {
+    "revision": "a83647918399fc2b5842",
+    "url": "/spotify-react-redux/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ded09768a2a0b07f3885",
+    "url": "/spotify-react-redux/static/css/main.40fa6621.chunk.css"
+  },
+  {
+    "revision": "a83647918399fc2b5842",
+    "url": "/spotify-react-redux/static/js/2.115bffe3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spotify-react-redux/static/js/2.115bffe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded09768a2a0b07f3885",
+    "url": "/spotify-react-redux/static/js/main.c6d429c5.chunk.js"
+  },
+  {
+    "revision": "59dc2f930db85d1031f4",
+    "url": "/spotify-react-redux/static/js/runtime-main.f8d15f87.js"
+  }
+]);
